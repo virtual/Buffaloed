50f1c3430461093ccb2f355478944033fc7d647a
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-10-17 14:01:46 -0600

    added background

--- client/src/attractions/Attractions.js
+++ client/src/attractions/Attractions.js
@@ -3,12 +3,14 @@ import CheckBox from './Checkbox';
 import StartButton from './Button';
 import FeatureMap from './Map';
 import Leaderboard from './Leaderboard';
+import FeatureCards from '../featurecards/FeatureCards';
 
 export default class Attractions extends Component {
   render () {
     return (
       <div>
         <h1>List of Attractions..coming soon</h1>
+        <FeatureCards />
         <CheckBox />
          <StartButton />
          <FeatureMap />
