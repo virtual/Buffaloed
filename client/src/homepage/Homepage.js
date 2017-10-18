6585548d18073cb0b4c2dd45dc847ea7304bc465
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-10-17 20:58:42 -0600

    remove quiz from homepage

--- client/src/homepage/Homepage.js
+++ client/src/homepage/Homepage.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import FeatureCards from '../featurecards/FeatureCards';
-import QuizBox from '../quiz/Quiz';
+import FeatureCards from '../featurecards/FeatureCards'; 
 
 export default class Homepage extends Component {
   render () {
     return (
-      <div>
-        <h1>Welcome!!</h1>
-        <FeatureCards />
-        <QuizBox/>
-
-        BELOW QUIZ BOX!!
+      <div> 
+        <FeatureCards /> 
       </div>
     );
   }
