d09e51a64437f6924845c790cd49ff3c4aef26f4
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-10-19 11:37:58 -0600

    build out sightinfo

--- client/src/sights/Map.js
+++ client/src/sights/Map.js
@@ -3,13 +3,28 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
  
 export default class FeatureMap extends Component { 
-    state = {
+  constructor(props) {
+    super(props);
+    this.state = {
       hasLocation: false,
       latlng: {
         lat: 46.5,
         lng: -111,
-      },
+      }
     }
+  }
+
+  componentDidMount() {
+    if(this.props.lat && this.props.lng) {
+      this.setState({
+        latlng: {
+          lat: this.props.lat,
+          lng: this.props.lng
+        }
+      })
+    }
+  }
+    
   
     handleClick = () => {
       this.refs.map.leafletElement.locate()
@@ -32,19 +47,22 @@ export default class FeatureMap extends Component {
       ) : null
   
       return (
-        <Map
-          center={this.state.latlng}
-          length={4}
-          onClick={this.handleClick}
-          onLocationfound={this.handleLocationFound}
-          ref="map"
-          zoom={10}>
-          <TileLayer
-            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          />
-          {marker}
-        </Map>
+        <div>
+          {this.state.latlng.lat} + {this.state.latlng.lng}
+          <Map
+            center={this.state.latlng}
+            length={4}
+            onClick={this.handleClick}
+            onLocationfound={this.handleLocationFound}
+            ref="map"
+            zoom={10}>
+            <TileLayer
+              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            />
+            {marker}
+          </Map>
+        </div>
       )
     }
   }
