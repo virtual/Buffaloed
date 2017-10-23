c61160141574f0ccbfedbf902108fd8a43ed2a2d
Emily Kimmel <kimmelem@hotmail.com>
2017-10-23 10:58:21 -0600

    text for homepage

--- client/src/homepage/Homepage.js
+++ client/src/homepage/Homepage.js
@@ -12,9 +12,11 @@ export default class Homepage extends Component {
         <Container textAlign='center'>
           <Header as='h2' icon>
         <Icon name='tree' size="huge" textAlign='center' />
-        Yellowstone
+        The Yellowstone Odyssey
         <Header.Subheader>
-          impress friends now!
+          You're planning a trip to Yellowstone, so we know you're smart-  but get even smarter by taking our Yellowstone Park quizzes and using our maps and Sights-descriptions to help plan your trip.
+          
+          As you prepare to wander through Yellowstone National Park, use the Yellowstone Odyssey to get even more inspired.  This app can help direct you to some of the more famous sights within the park.  In addition, you can impress your travelling companions with the Yellowstone-centric quizzes and use the interactive maps to keep yourself oriented while you visit the stunning and unique surroundings of the Park.
         </Header.Subheader>
         </Header>
         <br />
