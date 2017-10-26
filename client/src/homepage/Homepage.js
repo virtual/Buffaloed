06ded51f94db3747e26e863bff5a71e653faa554
Emily Kimmel <kimmelem@hotmail.com>
2017-10-26 13:04:49 -0600

    quiz questions

--- client/src/homepage/Homepage.js
+++ client/src/homepage/Homepage.js
@@ -16,7 +16,7 @@ export default class Homepage extends Component {
         <Header.Subheader>
           You're planning a trip to Yellowstone, so we know you're smart-  but get even smarter by taking our Yellowstone Park quizzes and using our maps and Sights-descriptions to help plan your trip.
           
-          As you prepare to wander through Yellowstone National Park, use the Yellowstone Odyssey to get even more inspired.  This app can help direct you to some of the more famous sights within the park.  In addition, you can impress your travelling companions with the Yellowstone-centric quizzes and use the interactive maps to keep yourself oriented while you visit the stunning and unique surroundings of the Park.
+          As you prepare to wander through Yellowstone National Park, use the Yellowstone Odyssey to get even more inspired.  This app can help direct you to some of the more famous sights within the park.  In addition, you can impress your traveling companions with the Yellowstone-centric quizzes and use the interactive maps to keep yourself oriented while you visit the stunning and unique surroundings of the Park.
         </Header.Subheader>
         </Header>
         <br />
